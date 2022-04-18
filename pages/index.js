5a131f46ef5d26ded44bacd1f33cc7add0e66177
007steve <stephen.plummer1.sp@gmail.com>
2022-04-18 09:19:40 -0400

    add chat

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Sidebar from '../components/Sidebar'
+import Chat from '../components/Chat'
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       </Head>
 
       <Sidebar/>
+      <Chat/>
     </div>
   )
 }
