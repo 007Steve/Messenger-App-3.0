c77baa39a10c9e97fa45e13a84f610c95a68d1a6
007steve <stephen.plummer1.sp@gmail.com>
2022-05-15 10:11:53 -0400

    updated icons

--- components/Chat.js
+++ components/Chat.js
@@ -1,20 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/Chat.module.css'
-import AttachFileIcon from '@mui/icons-material/AttachFile';
-import SendIcon from '@mui/icons-material/Send';
-import MicIcon from '@mui/icons-material/Mic';
+import { Avatar } from '@mui/material';
+import ArrowDropDownRoundedIcon from '@mui/icons-material/ArrowDropDownRounded';
+import ChatCard from './ChatCard';
+import MoreOptions from './MoreOptions';
 function Chat() {
+
+    const [show, setShow] = useState()
     return (
         <div className={styles.chat}>
-            <div className={styles.chat__navContainer}>Nav</div>
+            <div className={styles.chat__navContainer}>
+                <div className={styles.chat__photoContainer}>
+                    <Avatar src="/avatar-1.jpg" />
+                    <div>
+                        <h4 className={styles.chat__name}>Name</h4>
+                    </div>
+                </div>
+
+                <div>
+                    {show ? <MoreOptions /> : <></>}
+
+                    <ArrowDropDownRoundedIcon className={styles.chat__icons} fontSize="small" onClick={() => setShow(!show)} />
+                </div>
+            </div>
             <div className={styles.chat__messageContainer}>
+                <ChatCard  sender="sender" color="color"/>
+                <ChatCard />
+                <ChatCard />
+                <ChatCard  sender="sender" color="color"/>
+                <ChatCard />
+                <ChatCard  sender="sender" color="color"/>
+                <ChatCard />
+
+
             </div>
             <div className={styles.chat__inputContainer}>
                 <input className={styles.chat__input} placeholder="Search..."></input>
                 <div className={styles.chat__iconsContainer}>
-                    <MicIcon className={styles.chat__icons} fontSize="small" />
-                    <AttachFileIcon className={styles.chat__icons} fontSize="small"/>
-                    <SendIcon className={styles.chat__icons} fontSize="small"/>
+                   
+                    <img className={styles.chat__icons} src="/microphone-2.svg" alt=""/>
+                    <img className={styles.chat__icons} src="/paperclip-2.svg" alt=""/>
+                    <img className={styles.chat__icons} src="/send-3.svg" alt=""/>
                 </div>
             </div>
         </div>
