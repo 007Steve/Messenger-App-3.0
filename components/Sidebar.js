cc265c5606ee3a65289639d9e5bacf420cdd2db4
007steve <stephen.plummer1.sp@gmail.com>
2022-05-15 10:01:31 -0400

    updated icons

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,34 +1,40 @@
 import React from 'react'
 import styles from '../styles/Sidebar.module.css'
-import BoltIcon from '@mui/icons-material/Bolt';
-import SettingsIcon from '@mui/icons-material/Settings';
-import SendRoundedIcon from '@mui/icons-material/SendRounded';
-import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
-import AppsRoundedIcon from '@mui/icons-material/AppsRounded';
-import CalendarMonthSharpIcon from '@mui/icons-material/CalendarMonthSharp';
-import SearchIcon from '@mui/icons-material/Search';
-import { Avatar } from '@mui/material';
 import MessageCard from './MessageCard';
+import Link from 'next/link';
 function Sidebar() {
     return (
         <div className={styles.sidebar}>
             <div className={styles.sidebar__Container}>
                 <div className={styles.sidebar__iconsContainer}>
                     <div className={styles.sidebar__logoContainer}>
-                        <Avatar src="/MLogo.png"  />
+                        <img className={styles.sidebar__logo} src="/klo.png" alt="" />
                         <div className={styles.sidebar__logoCenterContainer}>
-                            <BoltIcon className={styles.sidebar__icons} />
-                            <AppsRoundedIcon className={styles.sidebar__icons} />
-                            <AppsRoundedIcon className={styles.sidebar__icons} /><CalendarMonthSharpIcon className={styles.sidebar__icons} />
-                            <SettingsIcon className={styles.sidebar__icons} />
+                            <img src="/home-2.svg" className={styles.sidebar__icons} />
+                            <img src="/message.svg" className={styles.sidebar__icons} />
+                            <img src="/user-square.svg" className={styles.sidebar__icons} />
+                            <img src="/folder-minus.svg" className={styles.sidebar__icons} />
+                            <img src="/setting-2.svg" className={styles.sidebar__icons} />
                         </div>
-                        <Avatar />
+                        <Link href="/signin">
+                            <img src="/logout.svg" className={styles.sidebar__icons} />
+                        </Link>
                     </div>
                 </div>
                 <div className={styles.sidebar__messageContainer}>
-                    <div className={styles.sidebar__inputContainer}>
-                        <SearchIcon className={styles.input__icons} />
-                        <input placeholder="Search..." className={styles.sidebar__input}></input>
+                    <div className={styles.sidebar__messageContainer__top}>
+                        <div className={styles.sidebar__headerContainer}>
+                            <h3 className={styles.sidebar__headerContainerText}>Messages</h3>
+                            <div className={styles.sidebar__inputContainerIcons}>
+                                <img src="/edit.svg" className={styles.input__icons} />
+                                <img src="/setting-2.svg" className={styles.input__icons} />
+                            </div>
+
+                        </div>
+                        <div className={styles.sidebar__inputContainer}>
+                            <img src="/search-normal.svg" className={styles.input__icons} />
+                            <input placeholder="Search..." className={styles.sidebar__input}></input>
+                        </div>
                     </div>
                     <MessageCard />
                     <MessageCard />
@@ -36,6 +42,8 @@ function Sidebar() {
                     <MessageCard />
                     <MessageCard />
                     <MessageCard />
+                    <MessageCard />
+
                 </div>
             </div>
         </div>
